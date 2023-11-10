c46cbb42cd7fb1fd98acd7d7eada493f35ad56b9
shenuka-jayasinghe <shenukacj@outlook.com>
2023-11-10 23:14:22 +0000

    connect db to svelte frontend

---
+++ skeleton-view/src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl">cUltuRed.</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		<LightSwitch />
+
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ skeleton-view/src/routes/all-books/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from 'svelte'
+  
+    // @ts-ignore
+    /**
+	 * @type {any[]}
+	 */
+    let posts = []
+    onMount(() => {
+    fetch('http://localhost:5000/')
+      .then(response => {
+        return response.json()
+    })
+      .then(result => {
+        console.log(posts)
+        posts = result})
+  })
+</script>
+
+<div class="container m-3">
+    {#each posts as post (post.book_id)}
+  <h2>({post.title}) {post.author}</h2>
+
+  <!-- <p>{post.body}</p> -->
+  <hr>
+{/each}
+</div>
