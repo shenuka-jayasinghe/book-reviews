30c0170c3916620b48f3397e8332eca0daa2e75b
shenuka-jayasinghe <shenukacj@outlook.com>
2023-11-11 12:22:39 +0000

    add cards with images to /all-books endpoint

--- skeleton-view/src/routes/all-books/+page.svelte
+++ skeleton-view/src/routes/all-books/+page.svelte
@@ -1,27 +1,38 @@
 <script>
-    import { onMount } from 'svelte'
-  
-    // @ts-ignore
-    /**
+	import { onMount } from 'svelte';
+
+	// @ts-ignore
+	/**
 	 * @type {any[]}
 	 */
-    let posts = []
-    onMount(() => {
-    fetch('http://localhost:5000/')
-      .then(response => {
-        return response.json()
-    })
-      .then(result => {
-        console.log(posts)
-        posts = result})
-  })
+	let posts = [];
+	onMount(() => {
+		fetch('http://localhost:5000/')
+			.then((response) => {
+				return response.json();
+			})
+			.then((result) => {
+				console.log(posts);
+				posts = result;
+			});
+	});
+	import Card from '$lib/Card/Card.svelte';
+    let bookTitle = "Title";
+    let imagePath = "";
+
 </script>
 
-<div class="container m-3">
-    {#each posts as post (post.book_id)}
-  <h2>({post.title}) {post.author}</h2>
+<div class="container m-10">
+	<div class="container m-10 flex flex-wrap">
+        {#each posts as post (post.book_id)}
+		<!-- <h2>({post.title}) {post.author}</h2> -->
+        
+		<Card 
+        title={bookTitle = post.title}
+        image_path = { imagePath = post.image_path}
+        />
+		<hr />
+	{/each}
+    </div>
 
-  <!-- <p>{post.body}</p> -->
-  <hr>
-{/each}
 </div>
